6adb4955bc3ea292fe59c059993851f9d2cfde08
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-11-09 16:42:10 -0300

    refactor: add authentication usecase

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,13 +1,10 @@
-// import { AccountModel } from '../models/account-model'
+import { AccountModel } from '../models/account-model'
 
 export type AuthenticationParams = {
   email: string
   password: string
 }
 
-export type AccountModel = {
-  accessToken: string
-}
 export interface Authentication {
   auth (params: AuthenticationParams): Promise<AccountModel>
 }
