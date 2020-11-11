6a5d7fefe261bbfd7f08fe031bc4a3e037c4c7a5
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-11-11 11:40:11 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './spinner-styles.scss'
+
+type Props = React.HTMLAttributes<HTMLElement>
+
+const Spinner: React.FC<Props> = (props: Props) => {
+  return (
+    <div {...props} data-testid="spinner" className={[styles.spinner, props.className].join(' ')}>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+    </div>)
+}
+
+export default Spinner
